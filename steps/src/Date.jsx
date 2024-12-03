d2891a9f0d99d98e889001d09b7859d1fc61711e
Grifin Kyle <grifinkyle.asa@cvsu.edu.ph>
2024-12-03 15:57:34 +0800

    Added the date project'

--- steps/src/Date.jsx
+++ steps/src/Date.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 
+
+
 const Date = () => {
+    const newDate = new Date()
     const [isOpen, setIsOpen] = useState(true)
-    const [date, setDate] = useState(new Date())
+    const [count, setCount] = useState(0)
+    newDate.setDate(newDate.getDate() + count)
   return (
     <>
     <button className="close" onClick={()=>setIsOpen((prevState) => !prevState)}>&times;</button>
@@ -13,7 +17,7 @@ const Date = () => {
         <button style={{backgroundColor : '#7950f2', color: '#fff'}}>Next</button>
       </div>
 
-      <p className='message'>date</p>
+      <p className='message'></p>
 
       <div className="buttons">
         <button style={{backgroundColor : '#7950f2', color: '#fff'}}>Previous</button>
