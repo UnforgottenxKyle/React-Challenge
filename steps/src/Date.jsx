50d2631a3eda431bd6893cf73668d54e0bc6acf4
Grifin Kyle <grifinkyle.asa@cvsu.edu.ph>
2024-12-04 16:06:24 +0800

    adding a button to customize the date

--- steps/src/Date.jsx
+++ steps/src/Date.jsx
@@ -4,10 +4,10 @@ import { useState } from 'react'
 
 
 const Date = () => {
-    const newDate = new Date()
+    // const newDate = new Date();
     const [isOpen, setIsOpen] = useState(true)
     const [count, setCount] = useState(0)
-    newDate.setDate(newDate.getDate() + count)
+    // newDate.setDate(newDate.getDate() + count)
   return (
     <>
     <button className="close" onClick={()=>setIsOpen((prevState) => !prevState)}>&times;</button>
@@ -17,11 +17,11 @@ const Date = () => {
         <button style={{backgroundColor : '#7950f2', color: '#fff'}}>Next</button>
       </div>
 
-      <p className='message'></p>
+      <p className='message'>{count}</p>
 
       <div className="buttons">
-        <button style={{backgroundColor : '#7950f2', color: '#fff'}}>Previous</button>
-        <button style={{backgroundColor : '#7950f2', color: '#fff'}}>Next</button>
+        <button style={{backgroundColor : '#7950f2', color: '#fff'}} onClick={()=>setCount(c=>c-1)}>Previous</button>
+        <button style={{backgroundColor : '#7950f2', color: '#fff'}} onClick={()=>setCount(c=>c+1)}>Next</button>
       </div>
     </div>)}
 </> 
