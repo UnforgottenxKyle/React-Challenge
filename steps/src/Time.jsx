782218bc190e720cc7bdcb017b361da2ef5f0697
Grifin Kyle <grifinkyle.asa@cvsu.edu.ph>
2024-12-05 16:11:48 +0800

    Update the string

--- steps/src/Time.jsx
+++ steps/src/Time.jsx
@@ -6,7 +6,7 @@ import { useState } from 'react'
 const Time = () => {
     
     const [isOpen, setIsOpen] = useState(true)
-    const [count, setCount] = useState(1)
+    const [count, setCount] = useState(0)
     const [count2, setCount2] = useState(1)
     const date1 = new Date()
     date1.setDate(date1.getDate() + count)
@@ -20,7 +20,7 @@ const Time = () => {
         <button style={{backgroundColor : '#7950f2', color: '#fff'}} onClick={()=>setCount2(c=>c+2)}>Next</button>
       </div>
 
-      <p className='message'>{date1.toDateString()}</p>
+      <p className='message'> <span>{count === 0 ? "Today is" : count > 0 ? `${count} days from today is `: `${count} days ago was `}</span>{date1.toDateString()}</p>
 
       <div className="buttons">
         <button style={{backgroundColor : '#7950f2', color: '#fff'}} onClick={()=>setCount(c=>c-count2)}>Previous</button>
